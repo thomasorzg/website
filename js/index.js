ba119c3a3585c0ab553118c01c2fca4a779b0c17
thomasorzg <jaapagamerz12@gmail.com>
2019-04-16 01:38:32 -0700

    MYW

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const message = `Hi, I'm Thomas Orozco. I have a knowledge of Java ♨️, PHP and MySQL 🔥 technologies. I am passionate about learning and innovating mainly in the fun world of Minecraft 🌎.
+`;
+
+const container = document.querySelector('#target');
+let n;
+
+function rerun() {
+	container.textContent = '';
+	n = 0;
+	typist(message, container);
+};
+
+rerun();
+
+function interval(letter) {
+	console.log(letter);
+	if(letter == ';' || letter == '.' || letter == ',') {
+		return Math.floor((Math.random() * 500) + 500);
+	} else {
+		return Math.floor((Math.random() * 130) + 5);
+	}
+}
+
+function typist(text, target) {
+	if(typeof(text[n]) != 'undefined') {
+		target.textContent += text[n];
+	}
+	n++;
+	if(n < text.length) {
+		setTimeout(function() {
+			typist(text, target)
+		}, interval(text[n - 1]));
+	} 
+}
